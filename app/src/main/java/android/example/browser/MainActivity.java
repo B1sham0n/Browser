167ca535a309481edf264476ac56d89c4a095525
Valeriya <valeriya.ridchenko@mail.ru>
2019-07-07 21:20:26 +0300

    popup menu

--- app/src/main/java/android/example/browser/MainActivity.java
+++ app/src/main/java/android/example/browser/MainActivity.java
@@ -3,10 +3,21 @@ package android.example.browser;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +38,86 @@ public class MainActivity extends AppCompatActivity {
         btnMore = findViewById(R.id.btnMore);
         btnSearch = findViewById(R.id.btnSearch);
 
-        
+        swipeLayout.setOnRefreshListener(refreshListener);
+        btnSearch.setOnClickListener(searchListener);
+        btnMore.setOnClickListener(moreListener);
+
+        //TODO: добавить функционал кнопок в меню
+
+        //TODO: создать БД и сохранять home и marks
+    }
+    private String getAddress(){
+        String etOutput = etLink.getText().toString();
+        String search;
+        if(etOutput.contains("http"))
+            search = etOutput;
+        else
+            search = "https://" + etOutput;
+        return search;
+        //TODO: поиск по умолчанию в гугл (мб добавить отдельную кнопку)
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    private void openLink(String search){
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setAppCacheEnabled(true);
+        wv.setWebViewClient(new MyWebViewClient());
+
+        wv.loadUrl(search);
+
+        swipeLayout.setRefreshing(true);
+    }
+    SwipeRefreshLayout.OnRefreshListener refreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            wv.reload();
+            wv.loadUrl( "javascript:window.location.reload( true )" );
+        }
+    };
+    View.OnClickListener searchListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String link = getAddress();
+            openLink(link);
+        }
+    };
+    View.OnClickListener moreListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            LayoutInflater layoutInflater
+                    = (LayoutInflater) getBaseContext()
+                    .getSystemService(LAYOUT_INFLATER_SERVICE);
+
+            final View popupView = layoutInflater.inflate(R.layout.popup, null);
+            final PopupWindow popupWindow = new PopupWindow(
+                    popupView,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, true);
+
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setFocusable(true);
+
+            //popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
+            View parent = view.getRootView();
+
+            popupWindow.showAtLocation(parent, Gravity.RIGHT, 0, 0);
+
+        }
+    };
+}
+    class MyWebViewClient extends WebViewClient {
+    //без переопределения класса не будет открываться мой браузер
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
+    }
+
+    // Для старых устройств
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
     }
 }
