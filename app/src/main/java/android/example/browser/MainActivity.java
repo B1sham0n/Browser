ef16000e104f2317440e16de8da5d67a72e3d9f0
Valeriya <valeriya.ridchenko@mail.ru>
2019-07-07 19:15:03 +0300

    first commit

---
+++ app/src/main/java/android/example/browser/MainActivity.java
@@ -0,0 +1,32 @@
+package android.example.browser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    WebView wv;
+    SwipeRefreshLayout swipeLayout;
+    EditText etLink;
+    Button btnSearch, btnMore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = findViewById(R.id.webView);
+        swipeLayout = findViewById(R.id.swipe);
+        etLink = findViewById(R.id.etLink);
+        btnMore = findViewById(R.id.btnMore);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        
+    }
+}
